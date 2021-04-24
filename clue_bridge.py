eb3e69fc00024f1a508b57499d6c8cc91f23dd92
Larry Priest <larrytpriest@gmail.com>
2021-04-23 22:22:12 -0600

    updates with error log for WIFI error

--- clue_bridge.py
+++ clue_bridge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# This example scans for any BLE advertisements and prints one advertisement and one scan
-#response from every device found. This scan is more detailed than the simple test because
-# it includes specialty advertising types.  from:
+# This example scans for any BLE advertisements and prints one advertisement and
+# one scan response from every device found. This scan is more detailed than the
+# simple test because it includes specialty advertising types.  from:
 # https://circuitpython.readthedocs.io/projects/ble/en/latest/examples.html#detailed-scan
 # started on nov 23/2020 - LTP
 
@@ -11,7 +11,7 @@ import time
 from secrets import secrets
 import requests
 import adafruit_ble_broadcastnet
-import adafruit_ble
+# import adafruit_ble
 import struct
 import msmts  # msmts contains a dict of measurement codes and data formats.
 # Get my list of measurement codes and sensors.
@@ -35,7 +35,7 @@ def aio_post(path, **kwargs):
     temp_response = False
     try:
         temp_response = requests.post(aio_base_url + path, **kwargs)
-    except:
+    except requests.exceptions.ConnectionError:
         print('No joy in post ville')
     return temp_response
 
@@ -98,9 +98,9 @@ def convert_to_feed_data(values, attribute_name, attribute_instance):
         key = attribute_name.replace("_", "-") + "-" + str(i)
         if isinstance(value, tuple):
             for j in range(attribute_instance.element_count):
-                feed_data.append({"key": key + "-" + attribute_instance.field_names[j], 
-                                "value": value[j], }
-                )
+                feed_data.append({"key": key + "-" + attribute_instance.field_names[j],
+                                  "value": value[j], }
+                                 )
         else:
             feed_data.append({"key": key, "value": value})
     return feed_data
@@ -111,8 +111,6 @@ def convert_to_feed_data(values, attribute_name, attribute_instance):
 def retrieve_existing_feeds():
     """Retrieve existing feeds from io.adafruit. """
 
-    found = set()
-    scan_responses = set()
     print("Fetching existing feeds.")
     existing_feeds = {}
     response = aio_get("/groups")
@@ -135,104 +133,115 @@ def retrieve_existing_feeds():
     print("existing feeds:\n", existing_feeds)
     return existing_feeds
 
-class collect_data():
-    pass
 
-sequence_numbers = {}  # does not have to be a list
-
-existing_feeds = retrieve_existing_feeds()
-print("scanning for sensors")
-while True:
-
-    try:
-        for advertisement in ble.start_scan():  # this one sort of works
-            sensor_address = advertisement.address.string.replace(":", "")
-            if sensor_address in macaddr:
-                print("found: ", sensor_address)
-
-#                 if advertisement.scan_response and sensor_address not in scan_responses:
-#                     scan_responses.add(sensor_address)
-#                 elif not advertisement.scan_response and sensor_address not in found:
-#                     found.add(sensor_address)
-#                 else:
-#                     pass
-
-            # prepare for data extraction
-                feed_data = []
-                try:   # try this except KeyError i.e. if the data_dict[255] not exixt
-                    start = 1  # because 0 (the first byte of the data_dict is wrong
-                    dd = advertisement.data_dict[255]  # key[255] is data fleld
-                    sm: bytes = dd[start:len(dd)]  # extract as bytes of dd
-                    # the last entry of dd is a sequence number(2bytes for code one sn)
-                    while len(sm) > 3:  
-                        start_index = sm[1]  # total bytes
-                        fd = sm[2], sm[3]  # first measurment code
-                        tt = bytes(fd).hex()  # convert bytes to hex
-                        try:
-                            mt = trans_meas[tt][0]  # check to see if valid code
-                        except KeyError:
-                            print("measurement not found")
-                        else:  # get the data format & extract the data correctly
-                            val = struct.unpack(trans_meas[tt][1], bytes(sm[4:(sm[1]+2)]))
-                            if len(val) == 1:
-                                # checking seq  **need to fix the sqn and missednum stuff.
-                                if trans_meas[tt][1] == 'B':
-                                    sn = int(val[0])
-                                    print('Seq.#: ', sn)
-                                else:
-                                    val = val[0]
-                                    feed_data.append({"key": mt, "value": val})
-                            else:
-                                var = ("x", "y", "z")
-                                for x in range(len(val)):
-                                    feed_data.append(
-                                        {"key": mt+'-'+var[x], 'value': val[x]})
-
-                        sm = sm[(start + sm[1]):len(sm)]  # drop the processed data 
-
-                except KeyError:
-                    pass
-                data = feed_data
-                if sensor_address not in sequence_numbers:
-                    # ~ sequence_numbers[sensor_address] = sn
-                    number_missed = 0
+def collect_data(dd):
+    print('dd: ', dd)
+    feed_data = []
+    start = 5  # because 0 (the first byte of the data_dict is wrong
+    sm: bytes = dd[start:len(dd)]
+    # the last entry of dd is a sequence number(2bytes for code one sn)
+    while len(sm) > 3:
+        fd = sm[2], sm[3]  # first measurment code
+        print("fd ", fd)
+        tt = bytes(fd).hex()  # convert bytes to hex
+        try:
+            mt = trans_meas[tt][0]  # check to see if valid code
+        except KeyError:
+            print("measurement not found")
+        else:  # get the data format & extract the data correctly
+            val = struct.unpack(trans_meas[tt][1], bytes(sm[4:(sm[1]+2)]))
+            if len(val) == 1:
+                # checking seq  **need to fix the sqn and missednum stuff.
+                if trans_meas[tt][1] == 'B':
+                    sn = int(val[0])
+                    print('Seq.#: ', sn)
                 else:
-                    if not (sn == (sequence_numbers[sensor_address] + 1)):
-                        number_missed = sn - sequence_numbers[sensor_address]
-                        print('We have missed: ', number_missed, 'packets')
-                        print('old seq. number:', sequence_numbers[sensor_address],
-                              'new seq. # :', sn)
-                        
-                        number_missed = 0
-                start_time = time.monotonic()
-                group_key = "bridge-{}-sensor-{}".format(bridge_address, sensor_address)
-                if sensor_address not in existing_feeds:
-                    print("sensor not in existing feeds")
-                    create_group("Bridge {} Sensor {}".format(
-                                                        bridge_address, sensor_address))
-                    create_feed(group_key, "Missed Message Count")
-                    existing_feeds[sensor_address] = ["missed-message-count"]
-                
-                for feed_data in data:
-                    if feed_data["key"] not in existing_feeds[sensor_address]:
-                        print("creating feed", feed_data['key'])
-                        create_feed(group_key, feed_data["key"])
-                        existing_feeds[sensor_address].append(feed_data["key"])
+                    val = val[0]
+                    feed_data.append({"key": mt, "value": val})
+            else:
+                var = ("x", "y", "z")
+                for x in range(len(val)):
+                    feed_data.append(
+                        {"key": mt+'-'+var[x], 'value': val[x]})
 
-        # Only update the previous sequence if we logged successfully.
-                if create_data(group_key, data):
-                    sequence_numbers[sensor_address] = sn
-                duration = time.monotonic() - start_time
-                print("Done logging msmts. to IO. Took {:.6f} seconds".format(duration))
-                print()
-    except KeyboardInterrupt:
-        print(' \n \n')
-        break
+        sm = sm[(start + sm[1]):len(sm)]  # drop the processed data
+        return feed_data
 
+
+def main():
+    existing_feeds = retrieve_existing_feeds()
+    sequence_numbers = {}
+    print("scanning for sensors")
+    scan_responses = {}
+    found = ()
+    # sequence_number = 0  # does not have to be a list
+    while True:
+
+        try:
+            for advertisement in ble.start_scan():  # this one sort of works
+                sensor_address = advertisement.address.string.replace(":", "")
+                if sensor_address in macaddr:
+                    print("found: ", sensor_address)
+                    print('bytes ', advertisement.data_dict)
+
+                    # if sensor_address not in scan_responses:
+                    #     scan_responses.add(sensor_address)
+                    # elif not advertisement.scan_response and sensor_address not in found:
+                    #     found.add(sensor_address)
+
+                # prepare for data extraction
+                    feed_data = []
+                    try:   # try this except KeyError i.e. if the data_dict[255] not exixt
+                        dd = advertisement.data_dict[255]  # key[255] is data fleld
+                        data = collect_data(dd)
+                    except KeyError:
+                        continue
+                    print('data: \n', data)
+                    #
+                    # if sensor_address not in sequence_numbers:
+                    #     # ~ sequence_numbers[sensor_address] = sn
+                    #     number_missed = 0
+                    # else:
+                    #     if not (feed_datvalue'sn == (sequence_numbers[sensor_address] + 1)):
+                    #         number_missed = sn - sequence_numbers[sensor_address]
+                    #         print('We have missed: ', number_missed, 'packets')
+                    #         print('old seq. number:', sequence_numbers[sensor_address],
+                    #               'new seq. # :', sn)
+                    #
+                    #         number_missed = 0
+                    start_time = time.monotonic()
+                    group_key = "bridge-{}-sensor-{}".format(bridge_address, sensor_address)
+                    if sensor_address not in existing_feeds:
+                        print("sensor not in existing feeds")
+                        create_group("Bridge {} Sensor {}".format(
+                            bridge_address, sensor_address))
+                        create_feed(group_key, "Missed Message Count")
+                        existing_feeds[sensor_address] = ["missed-message-count"]
+
+                    for feed_data in data:
+                        if feed_data["key"] not in existing_feeds[sensor_address]:
+                            print("creating feed", feed_data['key'])
+                            create_feed(group_key, feed_data["key"])
+                            existing_feeds[sensor_address].append(feed_data["key"])
+
+            # Only update the previous sequence if we logged successfully.
+                    if create_data(group_key, data):
+                        sequence_numbers[sensor_address] = sn
+                    duration = time.monotonic() - start_time
+                    print("Done logging msmts. to IO. Took {:.6f} seconds".format(duration))
+
+                    print()
+        except KeyboardInterrupt:
+            print(' \n \n')
+            break
+
+
+if __name__ == '__main__':
+    main()
 print("scan done")
 ble.stop_scan()
 # print("found:\n", found)
-print()
-print("responses:\n", scan_responses)
-print("sequence_numbers:\n", sequence_numbers)
+print('End It')
+# print("responses:\n", scan_responses)
+# print("sequence_numbers:\n", sequence_numbers)
 sys.exit()
--- msmts.py
+++ msmts.py
@@ -7,10 +7,10 @@ used for receiving data from centrals on BLE
 '''
 
 measure = {
-            '040a': ("temperature", "f"),
-            '010a': ("magnetic", "fff"),
-            '0300': ("sequence-number", "B"),
-            '0a0a': ("pressure", "f"),
-            '0b0a': ("relative-humidity", "f"),
-            '000a': ("acceleration", "fff")
-          }
+    '040a': ("temperature", "f"),
+    '010a': ("magnetic", "fff"),
+    '0300': ("sequence-number", "B"),
+    '0a0a': ("pressure", "f"),
+    '0b0a': ("relative-humidity", "f"),
+    '000a': ("acceleration", "fff")
+}
